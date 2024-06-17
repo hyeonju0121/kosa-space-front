fa94354f3109d284a71005a804efa5f983bc9953
seongminQa <znznfltm@gmail.com>
2024-06-18 01:17:49 +0900

    Feat: 개인별 데일리 과제 목록 조회 컴포넌트 구성

--- src/views/Admin/Management/Learning/DailyNote/components/PersonalNoteList.vue
+++ src/views/Admin/Management/Learning/DailyNote/components/PersonalNoteList.vue
@@ -1,19 +1,81 @@
 <template>
     <!-- contents -->
-	<div class="headingArea">
-		<div class="title">
-			<h1 id="itemTitle">개인별 데일리 과제 목록 조회</h1>
-		</div>
-
-        <div>
-            <BaseButtonUpdate class="mt-3" @click="handleDailyNoteBtn">과제 상세 보기</BaseButtonUpdate>
+    <div class="headingArea">
+        <div class="title">
+            <h1 id="itemTitle">개인별 데일리 과제 목록 조회</h1>
         </div>
-	</div>
+        <!--  -->
+        <PersonalProfileHeader class="mb-3" />
+        <div class="d-flex mb-4">
+            <input class="me-3" type="date" id="" name="" min="" value="">
+            <span class="me-3">~</span>
+            <input type="date" id="" name="" min="" value="">
+        </div>
+        <div class="table">
+            <table class="" style="width: 1000px;">
+                <thead class="text-center align-middle" style="height: 70px;">
+                    <th style="width: 15%">이름</th>
+                    <th style="width: 15%">교육생 번호</th>
+                    <th style="width: 30%">제출여부</th>
+                    <th style="width: 25%">상세보기</th>
+                    <th style="width: 15%">주차별 태그</th>
+                </thead>
+                <tbody class="text-center align-middle">
+                    <tr style="border-bottom:1px solid #dcdcdc; height: 50px">
+                        <td>유팀장</td>
+                        <td>M2001</td>
+                        <td><span class="btn btn-dark">제출</span></td>
+                        <td>
+                            <RouterLink to="/admin/dailynote/trainee/note/detail" class="btn btn-outline-dark btn-sm">과정
+                                상세보기</RouterLink>
+                        </td>
+                        <td>
+                            <RouterLink to="" class="btn btn-primary btn-sm">1주차</RouterLink>
+                        </td>
+                    </tr>
+                    <tr style="border-bottom:1px solid #dcdcdc; height: 50px">
+                        <td>유팀장</td>
+                        <td>M2001</td>
+                        <td><span class="btn btn-dark">제출</span></td>
+                        <td>
+                            <RouterLink to="/admin/dailynote/trainee/note/detail" class="btn btn-outline-dark btn-sm">과정
+                                상세보기</RouterLink>
+                        </td>
+                        <td>
+                            <RouterLink to="" class="btn btn-primary btn-sm">2주차</RouterLink>
+                        </td>
+                    </tr>
+                    <tr style="border-bottom:1px solid #dcdcdc; height: 50px">
+                        <td>유팀장</td>
+                        <td>M2001</td>
+                        <td><span class="btn btn-dark">미제출</span></td>
+                        <td>
+                            <RouterLink to="/admin/dailynote/trainee/note/detail" class="btn btn-outline-dark btn-sm">과정
+                                상세보기</RouterLink>
+                        </td>
+                        <td>
+                            <RouterLink to="" class="btn btn-primary btn-sm">3주차</RouterLink>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <td colspan="6" class="text-center" style="height: 50px">
+                        <b class="text-danger">페이지네이션 추가</b>
+                    </td>
+                </tfoot>
+            </table>
+        </div>
+
+    </div>
+    <div>
+        <BaseButtonUpdate class="mt-3" @click="handleDailyNoteBtn">과제 상세 보기</BaseButtonUpdate>
+    </div>
 </template>
 
 <script setup>
 import BaseButtonUpdate from '@/components/UIComponents/BaseButtonUpdate.vue';
 import { useRouter } from 'vue-router';
+import PersonalProfileHeader from '@/components/UIComponents/PersonalProfileHeader.vue'
 
 const router = useRouter();
 
@@ -36,8 +98,7 @@ function handleDailyNoteBtn() {
 }
 
 #itemTitle {
-	font-weight: 700;
+    font-weight: 700;
     font-size: 1.6rem;
 }
-
 </style>
