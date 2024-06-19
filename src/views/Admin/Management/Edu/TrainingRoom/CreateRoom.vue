a088cbe19d60a7701f2d15ba71441d2669b997d9
yu hyun ju <a01085249773@gmail.com>
2024-06-19 20:21:51 +0900

    Feat: Admin 교육관리 강의실 등록, 목록, 수정 컴포넌트 구현

--- src/views/Admin/Management/Edu/TrainingRoom/CreateRoom.vue
+++ src/views/Admin/Management/Edu/TrainingRoom/CreateRoom.vue
@@ -9,11 +9,66 @@
             <div class="title">
                 <h1 id="itemTitle">강의실 등록</h1>
             </div>
+        </div>
 
-            <div class="btn_big_wrap">
-                <BaseButtonCancle @click="handleCancle">취소</BaseButtonCancle>
-                <BaseButtonSubmit @click="handleSubmit">완료</BaseButtonSubmit>
-            </div>
+        <div class="form_table no_line">
+            <form>
+                <div class="tr">
+                    <div class="th" style="width: 20%;">
+                        <p class="form_label">교육장 명</p>
+                    </div>
+                    <div class="td">
+                        <select id="room" style="margin-top: 10px;">
+                            <option value="option1">송파 교육장</option>
+                            <option value="option2">가산 교육장</option>
+                            <option value="option3">혜화 교육장</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="tr">
+                    <div class="th">
+                        <p class="form_label">강의실 명 </p>
+                    </div>
+                    <div class="textarea_group_title sm">
+                        <textarea id="tname" title="강의실 명 입력" placeholder="강의실 명을 입력해주세요." maxlength="50"></textarea>
+                        <p class="form_bytes"><span class="byte">0</span>/100</p>
+                    </div>
+                </div>
+                <div class="tr">
+                    <div class="th">
+                        <p class="form_label">수용 가능 인원</p>
+                    </div>
+                    <div class="textarea_group_title sm">
+                        <textarea id="tcapacity" title="수용 가능 인원 입력" placeholder="수용 가능 인원을 입력해주세요."
+                            maxlength="50"></textarea>
+                        <p class="form_bytes"><span class="byte">0</span>/100</p>
+                    </div>
+                </div>
+                <div class="tr">
+                    <div class="th" style="width:30%">
+                        <p class="form_label">사용 가능 여부 </p>
+                    </div>
+                    <div style="margin-top: 34px; display: flex;">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="tenable" id="tenable1">
+                            <label class="form-check-label" for="tenable1">
+                                가능
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="tenable" id="tenable2" checked>
+                            <label class="form-check-label" for="tenable2">
+                                불가능
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="btn_big_wrap">
+                    <BaseButtonCancle @click="handleCancle">취소</BaseButtonCancle>
+                    <BaseButtonSubmit @click="handleSubmit">완료</BaseButtonSubmit>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -37,10 +92,22 @@ function handleSubmit() {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
 
-body, h1, h2, h3, h4, h5, h6, input,
-p, span, small, textarea, select {
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+input,
+p,
+span,
+small,
+textarea,
+select {
     font-family: 'Noto Sans KR', sans-serif;
 }
+
 
 .headingArea {
     position: relative;
@@ -53,8 +120,146 @@ p, span, small, textarea, select {
 }
 
 #itemTitle {
-	font-weight: 700;
+    font-weight: 700;
     font-size: 1.6rem;
+}
+
+.btn_big_wrap {
+    box-sizing: border-box;
+    margin: 0;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    padding: 0;
+    margin-top: 60px;
+}
+
+.form_table {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    display: table;
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+    border-top: none;
+}
+
+.writeNoticeForm .tr {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    border-bottom: 1px solid #f2f2f2;
+}
+
+.tr .th {
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    display: table-cell;
+    padding: 20px 0;
+    vertical-align: top;
+    align-items: center;
+    width: 160px;
+}
+
+.form_label {
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-size: 15px;
+    font-weight: 500;
+    height: 48px;
+    margin-top: 0;
+    display: flex;
+    align-items: center;
+}
+
+.tr {
+    line-height: normal;
+    font-size: 14px;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    border-bottom: 1px solid #f2f2f2;
+}
+
+.tr .td {
+    color: var(--txt-color);
+    border-collapse: collapse;
+    box-sizing: border-box;
+    display: table-cell;
+    padding: 20px 0;
+    vertical-align: top;
+    flex: 1;
+    margin: 0;
+    margin-left: 30px;
+}
+
+
+/* 교육장 이름 입력 폼 ----------------------------*/
+.textarea_group_title {
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    border: 1px solid #cccccc;
+    border-radius: 3px;
+    padding: 15px 10px;
+    min-height: 102px;
+}
+
+.textarea_group_title textarea {
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    font-weight: 500;
+    width: 100%;
+    height: 48px;
+    font-size: 14px;
+    background: #f0f4fd;
+    border-radius: 3px;
+    transition: all 0.5s;
+    vertical-align: middle;
+    resize: none;
+    padding: 0 10px 0 5px;
+    border: 0;
+    min-height: 42px;
+}
+
+.form_bytes {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    width: 100%;
+    text-align: right;
+    font-size: 12px;
+    color: #cccccc;
+    margin-top: 10px;
+    padding: 0px 5px;
+}
+
+.form_bytes .byte {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    text-align: right;
+    font-size: 12px;
+    color: #cccccc;
+    box-sizing: border-box;
 }
 
 .btn_big_wrap {
--- src/views/Admin/Management/Edu/TrainingRoom/RoomList.vue
+++ src/views/Admin/Management/Edu/TrainingRoom/RoomList.vue
@@ -10,13 +10,81 @@
                 <h1 id="itemTitle">강의실 목록</h1>
             </div>
         </div>
+
         <div>
-            <BaseButtonCreate class="mt-3" @click="handleCreateBtn">강의실 등록</BaseButtonCreate>
-            
-            <div class="btn_big_wrap">
-                <BaseButtonUpdate @click="handleUpdateBtn">수정</BaseButtonUpdate>
-                <BaseButtonCancle style="margin-left: 10px;">삭제</BaseButtonCancle>
-            </div>
+            <select id="room">
+                <option value="option1">송파 교육장</option>
+                <option value="option2">가산 교육장</option>
+                <option value="option3">혜화 교육장</option>
+            </select>
+
+            <BaseButtonSubmit class="mt-3" @click="handleCreateBtn">강의실 등록</BaseButtonSubmit>
+        </div>
+
+        <div class="container mt-3">
+            <table class="styled-table">
+                <thead>
+                    <tr>
+                        <th scope="col" >No</th>
+                        <th scope="col" >강의실 명</th>
+                        <th scope="col">수용 가능 인원</th>
+                        <th scope="col">사용 가능 여부</th>
+                        <th scope="col">현재 진행중인 교육과정</th>
+                        <th scope="col">등록일</th>
+                        <th scope="col">비고</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <tr>
+                        <td>1</td>
+                        <td>L1</td>
+                        <td>34 명</td>
+                        <td>불가능</td>
+                        <td>MSA 기반 Full Stack 개발 전문가 양성과정 2차</td> 
+                        <td>2024-06-18 19:26</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-dark" @click="handleUpdateBtn">수정</button>
+                            <button type="button" class="btn btn-outline-dark">삭제</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>L2</td>
+                        <td>30 명</td>
+                        <td>불가능</td>
+                        <td>MSA 기반 Full Stack 개발 전문가 양성과정 2차</td> 
+                        <td>2024-06-18 19:26</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-dark" @click="handleUpdateBtn">수정</button>
+                            <button type="button" class="btn btn-outline-dark">삭제</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>L3</td>
+                        <td>25 명</td>
+                        <td>불가능</td>
+                        <td>MSA 기반 Full Stack 개발 전문가 양성과정 2차</td> 
+                        <td>2024-06-18 19:26</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-dark" @click="handleUpdateBtn">수정</button>
+                            <button type="button" class="btn btn-outline-dark">삭제</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>L4</td>
+                        <td>34 명</td>
+                        <td>가능</td>
+                        <td></td> 
+                        <td>2024-06-18 19:26</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-dark" @click="handleUpdateBtn">수정</button>
+                            <button type="button" class="btn btn-outline-dark">삭제</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
@@ -25,6 +93,7 @@
 import BaseButtonCreate from '@/components/UIComponents/BaseButtonCreate.vue';
 import BaseButtonUpdate from '@/components/UIComponents/BaseButtonUpdate.vue';
 import BaseButtonCancle from '@/components/UIComponents/BaseButtonCancle.vue';
+import BaseButtonSubmit from '@/components/UIComponents/BaseButtonSubmit.vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -36,15 +105,34 @@ function handleCreateBtn() {
 function handleUpdateBtn() {
     router.push('/admin/room/update');
 }
+
+
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
 
-body, h1, h2, h3, h4, h5, h6, input,
-p, span, small, textarea, select {
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+input,
+p,
+span,
+small,
+textarea,
+select, th, td {
     font-family: 'Noto Sans KR', sans-serif;
 }
+
+/* th, td 
+{
+  text-align: center; 
+  vertical-align: middle;
+} */
 
 .headingArea {
     position: relative;
@@ -57,7 +145,7 @@ p, span, small, textarea, select {
 }
 
 #itemTitle {
-	font-weight: 700;
+    font-weight: 700;
     font-size: 1.6rem;
 }
 
@@ -69,5 +157,57 @@ p, span, small, textarea, select {
     justify-content: center;
     padding: 0;
     margin-top: 60px;
+}
+
+/* table */
+.styled-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.styled-table thead tr {
+    background-color: #22C55E;
+    color: #ffffff;
+    text-align: left;
+}
+
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
+}
+
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+}
+
+.createbtn {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-weight: 500;
+    background: transparent;
+    cursor: pointer;
+    font-size: 12px;
+    height: 25px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 3px;
+    width: 50px;
+    color: white;
+    background-color: #232323;
+    border: 1px solid black;
+    margin-left: 10px;
 }
 </style>
