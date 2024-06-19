d3a2d812d2006a004d0a97e8ee56d25ef17c9dfc
yu hyun ju <a01085249773@gmail.com>
2024-06-19 20:20:29 +0900

    Feat: admin 교육장 등록, 수정  컴포넌트 구현 및 Daum PostCode  API 사용하여 길찾기 컴포넌트 구현

--- src/views/Admin/Management/Edu/EduCenter/UpdateCenter.vue
+++ src/views/Admin/Management/Edu/EduCenter/UpdateCenter.vue
@@ -10,9 +10,63 @@
                 <h1 id="itemTitle">교육장 수정</h1>
             </div>
 
-            <div class="btn_big_wrap">
-                <BaseButtonCancle @click="handleCancle">취소</BaseButtonCancle>
-                <BaseButtonSubmit @click="handleSubmit">완료</BaseButtonSubmit>
+            <div class="form_table no_line">
+                <form>
+                    <div class="tr">
+                        <div class="th">
+                            <p class="form_label required">교육장 명</p>
+                        </div>
+                        <div class="td">
+                            <div class="textarea_group_title sm">
+                                <textarea id="btitle" name="btitle" title="교육장 명 입력" placeholder="교육장 명을 입력해주세요."
+                                    maxlength="50"></textarea>
+                                <p class="form_bytes"><span class="byte">0</span>/100</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tr">
+                        <div class="th">
+                            <p class="form_label required">교육장 주소 </p>
+                        </div>
+                        <div class="td">
+                            <DaumPostCode2 />
+                        </div>
+                    </div>
+
+                    <div class="tr">
+                        <div class="th">
+                            <p class="form_label required">교육장 사진 </p>
+                        </div>
+                        <div class="td">
+                            <div class="center_wrap">
+                                <div class="center_attach">
+                                    <div class="img_box d-flex">
+                                        <div id="defaultImg">
+                                            <PhImage :size="32" color="#636462" weight="duotone" />
+                                        </div>
+                                    </div>
+                                    <div class="center_edit">
+                                        <p class="tit">교육장의 이미지를 등록해주세요.</p>
+                                        <div class="attach_wrap">
+                                            <p class="guide_txt">파일 1개당 10MB까지 첨부 가능합니다. (JPG, JPEG, PNG, GIF만 첨부 가능)</p>
+                                            <div>
+                                                <input id="battach" type="file" class="form-control-file mt-3"
+                                                    ref="battach" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                    <div class="btn_big_wrap">
+                        <BaseButtonCancle @click="handleCancle">취소</BaseButtonCancle>
+                        <BaseButtonSubmit @click="handleSubmit">완료</BaseButtonSubmit>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -21,6 +75,7 @@
 <script setup>
 import BaseButtonCancle from '@/components/UIComponents/BaseButtonCancle.vue';
 import BaseButtonSubmit from '@/components/UIComponents/BaseButtonSubmit.vue';
+import DaumPostCode2 from './DaumPostCode2.vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -37,8 +92,19 @@ function handleSubmit() {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
 
-body, h1, h2, h3, h4, h5, h6, input,
-p, span, small, textarea, select {
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+input,
+p,
+span,
+small,
+textarea,
+select {
     font-family: 'Noto Sans KR', sans-serif;
 }
 
@@ -53,7 +119,7 @@ p, span, small, textarea, select {
 }
 
 #itemTitle {
-	font-weight: 700;
+    font-weight: 700;
     font-size: 1.6rem;
 }
 
@@ -65,5 +131,231 @@ p, span, small, textarea, select {
     justify-content: center;
     padding: 0;
     margin-top: 60px;
+}
+
+.form_table {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    display: table;
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+    border-top: none;
+}
+
+.writeNoticeForm .tr {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    border-bottom: 1px solid #f2f2f2;
+}
+
+.tr .th {
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    display: table-cell;
+    padding: 20px 0;
+    vertical-align: top;
+    align-items: center;
+    width: 160px;
+}
+
+.form_label {
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-size: 15px;
+    font-weight: 500;
+    height: 48px;
+    margin-top: 0;
+    display: flex;
+    align-items: center;
+}
+
+.tr {
+    line-height: normal;
+    font-size: 14px;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    border-bottom: 1px solid #f2f2f2;
+}
+
+.tr .td {
+    color: var(--txt-color);
+    border-collapse: collapse;
+    box-sizing: border-box;
+    display: table-cell;
+    padding: 20px 0;
+    vertical-align: top;
+    flex: 1;
+    margin: 0;
+    margin-left: 30px;
+}
+
+
+/* 교육장 이름 입력 폼 ----------------------------*/
+.textarea_group_title {
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    border: 1px solid #cccccc;
+    border-radius: 3px;
+    padding: 15px 10px;
+    min-height: 102px;
+}
+
+.textarea_group_title textarea {
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    font-weight: 500;
+    width: 100%;
+    height: 48px;
+    font-size: 14px;
+    background: #f0f4fd;
+    border-radius: 3px;
+    transition: all 0.5s;
+    vertical-align: middle;
+    resize: none;
+    padding: 0 10px 0 5px;
+    border: 0;
+    min-height: 42px;
+}
+
+.form_bytes {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    width: 100%;
+    text-align: right;
+    font-size: 12px;
+    color: #cccccc;
+    margin-top: 10px;
+    padding: 0px 5px;
+}
+
+.form_bytes .byte {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    text-align: right;
+    font-size: 12px;
+    color: #cccccc;
+    box-sizing: border-box;
+}
+
+.btn_big_wrap {
+    box-sizing: border-box;
+    margin: 0;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    padding: 0;
+    margin-top: 60px;
+}
+
+
+/* educenter image -------------------------*/
+.center_wrap {
+    border-collapse: collapse;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    background: white;
+    min-height: 120px;
+}
+
+.center_attach {
+    line-height: normal;
+    font-size: 14px;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+}
+
+.img_box {
+    line-height: normal;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    text-decoration: none;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 100px;
+    padding-bottom: 0;
+    /* border-radius: 50%; */
+    background: #F2F2F2;
+    color: #b1b1b1;
+    font-size: 32px;
+    font-weight: 700;
+    text-transform: uppercase;
+}
+
+
+.center_edit {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    margin-left: 30px;
+}
+
+.tit {
+    line-height: normal;
+    font-size: 14px;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+/* product img */
+.attach_wrap {
+    line-height: normal;
+    font-size: 14px;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+.guide_txt {
+    line-height: normal;
+    font-weight: 500;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    margin-top: 5px;
+    padding-top: 2px;
+    font-size: 12px;
+    color: #999999;
 }
 </style>
