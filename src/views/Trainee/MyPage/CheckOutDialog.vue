2b107d6b53b06daac66a3c1f20553f57530600ec
yu hyun ju <a01085249773@gmail.com>
2024-06-20 19:56:19 +0900

    Feat: 모달 슬롯 구현, 부모 컴포넌트(대시보드)로 데이터 전달 및 이벤트 정의

---
+++ src/views/Trainee/MyPage/CheckOutDialog.vue
@@ -0,0 +1,58 @@
+<template>
+    <Dialog>
+        <!--템플릿 <slot> 자리에 들어갈 내용 정의-->
+        <template v-slot:header>
+            <p class="title">KOSA SPACE 출결 확인</p>
+        </template>
+        
+        <template v-slot:body>
+            <p class="contents">현재 시간은 오후 5시 50분입니다. (서버시간 기준)</p>
+            <p class="contents">퇴실시 재입실이 불가합니다.</p>
+            <p class="contents">퇴실처리하시겠습니까?</p>
+        </template>
+
+        <template v-slot:footer>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="margin-right: 5%;">아니오</button>
+            <!-- 자식 컴포넌트에서 모달 안에 제출 버튼을 클릭했을 때 처리하는 이벤트 정의-->
+            <button type="button" class="btn btn-primary" @click="emit('submitCheckOut')">네</button>
+        </template>
+
+    </Dialog>
+</template>
+
+<script setup>
+import Dialog from '@/components/UIComponents/Dialog.vue';
+
+const emit = defineEmits(["submitCheckOut"]);
+
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+input,
+p,
+span,
+small,
+textarea,
+select {
+    font-family: 'Noto Sans KR', sans-serif;
+}
+
+.title {
+    font-weight: 700;
+}
+
+.contents {
+    font-weight: 500;
+    font-size: 1.1rem;
+    text-align: center;
+}
+</style>
