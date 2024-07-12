bd1b3f03a0bd2fae6df40e32881eea4e114a20fd
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2024-07-13 02:51:53 +0900

    Refactor: 공지사항 컴포넌트 레이아웃 수정

--- src/views/Admin/Management/Notice/NoticeDetail.vue
+++ src/views/Admin/Management/Notice/NoticeDetail.vue
@@ -6,19 +6,64 @@
 
 
         <div class="headingArea">
-            <div class="title">
+            <div class="title1">
                 <h1 id="itemTitle">공지사항 상세 조회</h1>
             </div>
 
 
             <div class="container mt-5" style="text-align: left;">
+                <div class="detail-board-box">
+				<input type="hidden" id="boardSeq">
+				<div class="title-group">
+					<div class="lpart">
+						<span class="hidden-txt">제목</span>
+						<div class="title" id="title">{{ notice.ntitle }}</div>
+					</div>
+					<div class="rpart">
+						<div class="board-date">
+							<span class="hidden-txt">작성일</span> <span class="text" id="regDate">2022-07-07</span>
+						</div>
+					</div>
+				</div>
 
+                <div class="category" style="display: flex;">
+                    <div class="educenter">
+                        <p class="category-title">교육장</p>
+                        <h5 class="category-sub">송파교육센터</h5>
+                    </div>
+                    <div class="course" style="margin-left: 6%;">
+                        <p class="category-title2">교육과정</p>
+                        <h5 class="category-sub">MSA 2차 Full Stack 개발자 양성과정</h5>
+                    </div>
+                    <div class="type" style="margin-left: 6%;">
+                        <p class="category-title">유형</p>
+                        <h5 class="category-sub">취업</h5>
+                    </div>
+                    <div class="writer" style="margin-left: 6%;">
+                        <p class="category-title3">작성자</p>
+                        <h5 class="category-sub">kosaSB</h5>
+                    </div>
+                </div>
+
+
+				<div class="conts-group" id="boardCtt">
+                    <p style="line-height: 1.8;">
+                        금융결제원 CA 인증서 만료일 도래에 따라 CA인증서가 갱신되었음을&nbsp;안내드리오니 아래를 참고하시어 갱신된&nbsp;운영&nbsp;CA인증서를 적용하여 주시기 바랍니다.
+                        <br><br>
+                        <strong>1.&nbsp;갱신된&nbsp;운영 CA인증서<br></strong>
+                    </p>
+                    <table class="__se_tbl" style="border: 1px solid rgb(199, 199, 199); border-image: none;" border="0" cellspacing="0" cellpadding="0" _se2_tbl_template="4"><tbody><tr><td style="padding: 3px 4px 2px; width: 137px; height: 17px; text-align: left; color: rgb(102, 102, 102); font-weight: normal; border-right-color: rgb(231, 231, 231); border-right-width: 1px; border-right-style: solid; background-color: rgb(243, 243, 243);"><p align="center" style="line-height: 1.8;">&nbsp;구분</p></td><td style="padding: 3px 4px 2px; width: 266px; height: 17px; text-align: left; color: rgb(102, 102, 102); font-weight: normal; border-right-color: rgb(231, 231, 231); border-right-width: 1px; border-right-style: solid; background-color: rgb(243, 243, 243);"><p align="center" style="line-height: 1.8;">운영</p></td></tr><tr><td style="padding: 3px 4px 2px; width: 137px; height: 18px; color: rgb(102, 102, 102); border-top-color: rgb(231, 231, 231); border-right-color: rgb(231, 231, 231); border-top-width: 1px; border-right-width: 1px; border-top-style: solid; border-right-style: solid; background-color: rgb(255, 255, 255);"><p align="center" style="line-height: 1.8;">&nbsp;CN</p></td><td style="padding: 3px 4px 2px; width: 266px; height: 18px; color: rgb(102, 102, 102); border-top-color: rgb(231, 231, 231); border-right-color: rgb(231, 231, 231); border-top-width: 1px; border-right-width: 1px; border-top-style: solid; border-right-style: solid; background-color: rgb(255, 255, 255);"><p align="center" style="line-height: 1.8;">&nbsp;yessignCA Class 3</p></td></tr><tr><td style="padding: 3px 4px 2px; width: 137px; height: 18px; color: rgb(102, 102, 102); border-top-color: rgb(231, 231, 231); border-right-color: rgb(231, 231, 231); border-top-width: 1px; border-right-width: 1px; border-top-style: solid; border-right-style: solid; background-color: rgb(255, 255, 255);"><p align="center" style="line-height: 1.8;">&nbsp;유효기간</p></td><td style="padding: 3px 4px 2px; width: 266px; height: 18px; color: rgb(102, 102, 102); border-top-color: rgb(231, 231, 231); border-right-color: rgb(231, 231, 231); border-top-width: 1px; border-right-width: 1px; border-top-style: solid; border-right-style: solid; background-color: rgb(255, 255, 255);"><p align="center" style="line-height: 1.8;">2020-05-15 ~ 2030-05-15</p></td></tr><tr><td style="padding: 3px 4px 2px; width: 137px; height: 18px; color: rgb(102, 102, 102); border-top-color: rgb(231, 231, 231); border-right-color: rgb(231, 231, 231); border-top-width: 1px; border-right-width: 1px; border-top-style: solid; border-right-style: solid; background-color: rgb(255, 255, 255);"><p align="center" style="line-height: 1.8;">&nbsp;<strong>갱신일</strong></p></td><td style="padding: 3px 4px 2px; width: 266px; height: 18px; color: rgb(102, 102, 102); border-top-color: rgb(231, 231, 231); border-right-color: rgb(231, 231, 231); border-top-width: 1px; border-right-width: 1px; border-top-style: solid; border-right-style: solid; background-color: rgb(255, 255, 255);"><p align="center" style="text-align: center; line-height: 1.8;"><strong>2022년 8월 7일</strong></p></td></tr></tbody></table><p style="line-height: 1.8;"><br><strong>2. 조치사항<br></strong>&nbsp;&nbsp;① 인증서 프로그램(가입자설비, RA시스템, PKI 검증서버 등)에 갱신된 인증서* 적용<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; * 인증서 DN 및 일련번호 변경됨<br>&nbsp;&nbsp;② 인증서가 갱신되었을 때 디렉토리 서버에서 자동 다운로드 받을 수 있도록 가입자설비 수정<br><strong><span style="color: rgb(58, 50, 195);">&nbsp; </span><span style="color: rgb(58, 50, 195);">※ 상기 조치 사항은 전문보안업체에 문의하여 조치</span></strong><br><br><strong>3. 미조치시 발생가능한 오류 (예시)</strong><br>&nbsp;- ';인증서 검증에 실패했습니다. 디렉토리 주소가 없습니다';<br>&nbsp;- ';허용되지 않은 CA로부터 발급된 인증서입니다';<br>&nbsp;- ';전자서명 인증서 유효성 검사 중 오류가 발생했습니다'; <br>-&nbsp;&nbsp;인증서 로그인시 인증서가 보이지 않는 현상 등 <br><br><strong>4. 문의처 : yeskeycert@kftc.or.kr</strong>&nbsp;<br><br>감사합니다.&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><div class="info-file"><div class="title">첨부파일(<span class="file-num">1</span>건)</div><div class="list"><ul><li><a href="#" class="btn btn-txt-file" addfileseq="101206">금융결제원 운영 CA인증서(yessignCA Class 3).zip</a></li>NaN</ul></div></div></div>
+			</div>   
+                
+                <!--
                 <div class="mb-3">
                     <span
                         style="font-size: 2rem; border-bottom: 4px solid #22C55E; padding-bottom: 3px; font-weight: 500;">{{
                             notice.ntitle }}</span>
                 </div>
+                 -->
 
+                <!--
                 <div class="d-flex justify-content-between mb-1">
                     <div class="d-flex ms-2">
                         <span class="caregory">{{ notice.ncategory }}</span>
@@ -29,11 +74,13 @@
                         <span class="text-end">{{ notice.ncreatedat }}</span>
                     </div>
                 </div>
+                -->
 
-                <div class="p-2 mb-3" style="border-bottom: 1px solid #22C55E;">
+                <!--<div class="p-2 mb-3" style="border-bottom: 1px solid #22C55E;">-->
                     <!-- <input ref="nattach" type="file"> -->
-                </div>
+                <!--</div>-->
 
+                <!--
                 <div class="d-flex flex-column p-3 mb-3"
                     style="width: 100%; min-height: 460px; border: 1px solid #22C55E">
                     <span>
@@ -48,6 +95,7 @@
                 <div class="d-flex justify-content-end">
                     <button class="btn btn-outline-secondary me-3" @click="handleList()" value="목록">목록으로</button>
                 </div>
+            -->
             </div>
         </div>
     </div>
@@ -190,7 +238,8 @@ p,
 span,
 small,
 textarea,
-select {
+select,
+button {
     font-family: 'Noto Sans KR', sans-serif;
 }
 
@@ -200,7 +249,7 @@ select {
     margin: 0 0 20px;
 }
 
-.headingArea .title {
+.headingArea .title1 {
     border-bottom: 2px solid #2a3954;
 }
 
@@ -245,4 +294,120 @@ select {
     background-color: rgb(40, 167, 172);
     font-weight: 500;
 }
+
+.detail-board-box {
+    border-top: 1px solid #303030;
+    border-bottom: 1px solid #e0e0e0;
+}
+
+.detail-board-box .title-group {
+    display: table;
+    width: 100%;
+    padding: 30px 0;
+}
+
+.detail-board-box .title-group .lpart {
+    display: table-cell;
+    vertical-align: top;
+}
+
+.hidden-txt {
+    overflow: hidden;
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    font-size: 1px;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    white-space: nowrap;
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+}
+
+.detail-board-box .title-group .title {
+    padding-right: 20px;
+    font-size: 30px;
+    font-weight: 900;
+    word-break: keep-all;
+}
+
+.detail-board-box .title-group .rpart {
+    display: table-cell;
+    vertical-align: top;
+    min-width: 200px;
+    padding-top: 10px;
+    text-align: right;
+}
+
+.board-date {
+    display: inline-block;
+}
+
+.detail-board-box .conts-group {
+    padding: 34px 30px 30px;
+    background: #f7f7f9;
+}
+
+.detail-board-box .conts-group .info-file {
+    width: 100%;
+    padding: 16px 30px;
+    margin-top: 30px;
+    border: 1px solid #f5f5f5;
+    border-radius: 8px;
+    box-sizing: border-box;
+    font-size: 0;
+    background: #ffffff;
+}
+
+.detail-board-box .conts-group .info-file .title {
+    display: inline-block;
+    vertical-align: top;
+    width: 120px;
+    padding-right: 26px;
+    box-sizing: border-box;
+    font-size: 16px;
+}
+
+.detail-board-box .conts-group .info-file .list {
+    display: inline-block;
+    vertical-align: top;
+    width: calc(100% - 120px);
+}
+
+.detail-board-box .conts-group .info-file .list .btn.btn-txt-file {
+    text-align: left;
+}
+
+.btn.btn-txt-file {
+    font-size: 16px;
+    font-weight: 900;
+    text-decoration: underline;
+    color: #4b27e7;
+    background: #ffffff;
+}
+
+.category-title {
+    font-size: 0.8rem;
+    color: #d44439;
+    margin-bottom: 3%;
+}
+
+.category-title2 {
+    font-size: 0.8rem;
+    color: #d44439;
+    margin-bottom: 0.7%;
+}
+
+.category-title3 {
+    font-size: 0.8rem;
+    color: #d44439;
+    margin-bottom: 0.7%;
+}
+
+.category-sub {
+    font-size: 1.2rem;
+    color: #333232;
+}
+
 </style>
--- src/views/Admin/Management/Notice/NoticeList.vue
+++ src/views/Admin/Management/Notice/NoticeList.vue
@@ -1,18 +1,30 @@
 <template>
-    <div class="main" style="margin-left: 25px; width: 1200px;">
+    <div class="main p-3" style="margin-left: 25px; width: 1200px;">
       <div class="item-section mt-2 mb-2" style="font-size: 12px">
         <span>공지사항 관리 > 공지사항 목록</span>
       </div>
   
-      <!-- contents -->
-      <div class="headingArea" style="margin-bottom: 2%;">
-        <div class="title">
-          <h1 id="itemTitle">공지사항 목록</h1>
-        </div>
-      </div>
+      <!-- 
+      <article class="key_visual_area visual_customer">
+					<div class="txt_area">
+						<h2>공지사항</h2>
+						<p>코사스페이스에서 알리는 새소식을 안내해 드립니다.</p>
+					</div>
+					<figure class="img" title="고객센터 이미지">
+            <img src="@/assets/visual_customer.png" style="width: 200px; height: 150px;">
+          </figure>
+			</article>
+    -->
   
+    <!-- contents -->
+    <div class="headingArea">
+        <div class="title">
+            <h1 id="itemTitle">공지사항</h1>
+        </div>
+    </div>
+
       <form @submit.prevent="handleSubmitFilter1">
-        <div class="align" style="display: flex;">
+        <div class="align" style="display: flex; margin-bottom: 5%;">
           <div class="InpBox">
             <select title="교육장 선택" v-model="filter.ecname" :class="btnEnable">
               <option selected disabled :value="''">교육장 선택</option>
@@ -26,6 +38,7 @@
             </select>
           </div>
   
+          <!--
           <div class="InpBox" style="margin-left: 1%; width: 150px;">
             <select title="카테고리 선택" v-model="filter.ncategory" :class="btnEnable">
               <option selected disabled :value="''">카테고리 선택</option>
@@ -37,27 +50,48 @@
               <option value="행사">행사</option>
             </select>
           </div>
+        -->
         </div>
       </form>
-  
-      <div class="mt-3" style="text-align:right">
-        <input type="button" class="btn btn-dark btn-sm" value="등록" @click="handleCreateBtn">
-      </div>
+
+      <div class="search-head-sec">
+          <div class="search-title" id="listCnt"><p><b>총 <span class="search-num">{{ notices.length }}</span>개</b>의 글이 등록되어 있습니다</p></div>
+              <div class="search-input-box">
+                  <div class="search-input-group">
+                      <div class="lpart">
+                          <select id="columnName" class="dsgn-select" title="검색 구분 선택">
+                              <option value="">카테고리 선택</option>
+                              <option value="all">전체</option>
+                              <option value="출결">출결</option>
+                              <option value="수업">수업</option>
+                              <option value="강의실">강의실</option>
+                              <option value="취업">취업</option>
+                              <option value="행사">행사</option>
+                          </select>
+                      </div>
+                      <div class="rpart">
+                          <div class="input-box">
+                              <input type="text" id="columnValue" class="dsgn-input" placeholder="검색어를 입력하세요" value="" title="검색어 입력">
+                              <button type="button" class="btn btn-txtclear"><span class="icon-txtclear"><span class="hidden-txt">문구삭제</span></span></button>
+                          </div>
+                          <button type="button" id="searchBtn" class="btn btn-sub bg-main btn-search">검색</button>
+                          <button type="button" id="searchBtn" class="btn btn-sub bg-main btn-search" @click="handleCreateBtn">공지사항 등록</button>  
+                      </div>
+                  </div>
+              </div>
+          </div>
   
       <div class="mt-3">
-        <div class="notice-summary">
-          총 {{ notices.length }}개의 글이 등록되어 있습니다.
-        </div>
-        <div class="search-box">
-          <input type="text" placeholder="공지사항 검색">
-          <button class="search-button">
-            <img src="search-icon.png" alt="Search">
-          </button>
-        </div>
         <table class="notice-table">
           <tbody>
             <tr v-for="notice in notices" :key="notice.id" :class="{'highlight': notice.highlight}">
-              <td class="label">{{ notice.label }}</td>
+              <td class="label">{{ notice.nno }}</td>
+              <td class="label">
+                <span class="d-flex">
+                  <span class="new_mark2">전체</span>
+                  <span class="new_mark_complete ms-2">취업</span>
+                </span>
+              </td>
               <td class="title">{{ notice.title }}</td>
               <td class="date">{{ notice.date }}</td>
               <td class="views">{{ notice.views }}</td>
@@ -75,15 +109,15 @@
   const ecnames = ref(['교육장1', '교육장2', '교육장3']);
   const cnames = ref(['과정1', '과정2', '과정3']);
   const notices = ref([
-    { id: 1, label: '공지', title: '금융데이터거래소 이용약관 개정 안내', date: '2020-12-21', views: 31, highlight: true },
-    { id: 2, label: '공지', title: '금융권 데이터 유통 가이드(2020.10 개정)', date: '2020-10-29', views: 237, highlight: true },
-    { id: 3, label: '공지', title: '금융데이터거래소 및 데이터전문기관(금융보안원) 소개자료', date: '2020-09-15', views: 355, highlight: true },
-    { id: 4, label: '', title: '금융데이터거래소 이용약관 개정 안내', date: '2020-12-21', views: 31, highlight: false },
-    { id: 5, label: '', title: '금융권 데이터 유통 가이드(2020.10 개정)', date: '2020-10-29', views: 237, highlight: false },
-    { id: 6, label: '', title: '금융데이터거래소 및 데이터전문기관(금융보안원) 소개자료', date: '2020-09-15', views: 355, highlight: false },
-    { id: 7, label: '', title: '개인정보 처리방침 개정 안내', date: '2020-08-19', views: 137, highlight: false },
-    { id: 8, label: '', title: '금융데이터거래소 이용약관 변경 안내', date: '2020-08-05', views: 194, highlight: false },
-    { id: 9, label: '', title: '2020년 하반기 데이터바우처(AI데이터 가공 바우처) 사업 안내', date: '2020-07-16', views: 298, highlight: false },
+    { id: 1, nno: 1, title: '금융데이터거래소 이용약관 개정 안내', date: '2020-12-21', views: 31, highlight: true },
+    { id: 2, nno: 2, title: '금융권 데이터 유통 가이드(2020.10 개정)', date: '2020-10-29', views: 237, highlight: true },
+    { id: 3, nno: 3, title: '금융데이터거래소 및 데이터전문기관(금융보안원) 소개자료', date: '2020-09-15', views: 355, highlight: true },
+    { id: 4, nno: 4, title: '금융데이터거래소 이용약관 개정 안내', date: '2020-12-21', views: 31, highlight: false },
+    { id: 5, nno: 5, title: '금융권 데이터 유통 가이드(2020.10 개정)', date: '2020-10-29', views: 237, highlight: false },
+    { id: 6, nno: 6, title: '금융데이터거래소 및 데이터전문기관(금융보안원) 소개자료', date: '2020-09-15', views: 355, highlight: false },
+    { id: 7, nno: 7, title: '개인정보 처리방침 개정 안내', date: '2020-08-19', views: 137, highlight: false },
+    { id: 8, nno: 8, title: '금융데이터거래소 이용약관 변경 안내', date: '2020-08-05', views: 194, highlight: false },
+    { id: 9, nno: 9, title: '2020년 하반기 데이터바우처(AI데이터 가공 바우처) 사업 안내', date: '2020-07-16', views: 298, highlight: false },
   ]);
   
   const filter = ref({
@@ -123,7 +157,8 @@
   select,
   strong,
   th,
-  td {
+  td,
+  button {
     font-family: 'Noto Sans KR', sans-serif;
   }
   
@@ -198,6 +233,7 @@
   .notice-table {
     width: 100%;
     border-collapse: collapse;
+    border-top: 1px solid #37c375;
   }
   
   .notice-table tr {
@@ -205,7 +241,7 @@
   }
   
   .notice-table tr.highlight {
-    background-color: #f0f8ff;
+    background-color: #f2fdf7;
   }
   
   .notice-table td {
@@ -216,7 +252,7 @@
     width: 5%;
     text-align: center;
     font-weight: bold;
-    color: #007bff;
+    color: #37c375;
   }
   
   .notice-table .title {
@@ -244,5 +280,199 @@
     padding: 5px 10px;
     font-size: 12px;
   }
+
+  .key_visual_area {
+    width: 1260px;
+    height: 170px;
+    border-radius: 10px;
+    margin: 0;
+    color: #fff;
+    padding: 35px 80px 35px 60px;
+    display: flex;
+    background: #37c375;
+    box-sizing: border-box;
+}
+
+.key_visual_area .txt_area {
+    display: flex;
+    flex-direction: column;
+}
+
+.key_visual_area h2 {
+    font-size: 35px;
+}
+
+.key_visual_area p {
+    font-size: 15px;
+    margin-top: 10px;
+    line-height: 1.3;
+}
+.key_visual_area.visual_customer .img {
+    width: 180px;
+    height: 133px;
+    background: transparent no-repeat 0 0;
+    background-size: 227px 200px;
+    position: absolute;
+    right: 420px;
+    top: 53px;
+}
+
+/* 검색 */
+.search-head-sec {
+    padding-bottom: 50px;
+}
+
+.search-head-sec .search-title {
+    padding-bottom: 30px;
+}
+
+.search-title {
+    text-align: center;
+    font-size: 30px;
+}
+
+.search-title .search-num {
+    color: #22C55E;
+}
+
+.search-title b {
+    font-weight: 900;
+}
+
+.search-input-group {
+    text-align: center;
+    font-size: 0;
+}
+
+.search-input-group .lpart {
+    display: inline-block;
+}
+
+.search-input-group .dsgn-select {
+    width: 280px;
+}
+
+.input-box {
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+}
+
+.dsgn-select {
+    width: 400px;
+    height: 54px;
+    padding: 0 47px 0 20px;
+    vertical-align: middle;
+    font-size: 16px;
+    color: #303030;
+    border-radius: 9px;
+    background: #f7f7f9 url(https://www.yeskey.or.kr/resources/image/component/form/select_arrow.svg) no-repeat right 20px center;
+}
+
+.dsgn-select {
+    appearance: none;
+    box-sizing: border-box;
+    border: none;
+    box-shadow: none;
+}
+
+.dsgn-input {
+    width: 400px;
+    height: 54px;
+    padding: 0 20px;
+    vertical-align: middle;
+    font-size: 16px;
+    color: #333333;
+    border-radius: 9px;
+    background: #f7f7f9;
+}
+
+.dsgn-input {
+    appearance: none;
+    box-sizing: border-box;
+    border: none;
+    box-shadow: none;
+}
+
+.search-input-group .rpart {
+    display: inline-block;
+    margin-left: 10px;
+}
+
+.input-box {
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+}
+
+.search-input-group .btn.btn-sub {
+    min-width: 80px;
+    margin-left: 10px;
+    font-weight: 500;
+}
+
+.input-box .btn-txtclear {
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    margin-top: -20px;
+}
+
+.btn.btn-txtclear {
+    display: none;
+    position: relative;
+    width: 40px;
+    height: 40px;
+}
+
+.btn.bg-main {
+    color: #ffffff;
+    border: 1px solid #22C55E;
+    background: #22C55E;
+}
+
+.btn.btn-sub {
+    min-width: 100px;
+    padding: 0 24px;
+    height: 50px;
+    line-height: 52px;
+    border-radius: 7px;
+}
+
+.btn {
+    vertical-align: middle;
+    position: relative;
+    box-sizing: border-box;
+    text-align: center;
+    color: #303030;
+    font-size: 16px;
+}
+
+/* tag */
+.new_mark_complete {
+    display: block;
+    width: 38px;
+    height: 24px;
+    border-radius: 4px;
+    color: rgb(235, 25, 39);
+    font-size: 13px;
+    font-weight: bold;
+    line-height: 24px;
+    text-align: center;
+    background-color: rgb(254, 239, 238);
+}
+
+.new_mark2 {
+    display: block;
+    width: 50px;
+    height: 24px;
+    border-radius: 4px;
+    color: #2a7c6d;
+    font-size: 13px;
+    font-weight: bold;
+    line-height: 24px;
+    text-align: center;
+    background-color: #defaf6;
+}
   </style>
   
